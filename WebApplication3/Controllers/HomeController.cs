66298c0f3d7ad8560d0f80c4e273c8e04ae4d08c
johndward01 <johndward01@gmail.com>
2022-08-08 17:19:31 -0500

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var client = new HttpClient();
+        var kanye = new KanyeResponse();
+        var kanyeResponse = client.GetStringAsync("https://api.kanye.rest").Result;
+        var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+        kanye.Response = kanyeQuote;
+        return View(kanye);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ WebApplication3/Models/KanyeResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication3.Models;
+
+public class KanyeResponse
+{
+    public string Response { get; set; }
+
+}
